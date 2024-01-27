a4835edb426a24f1fe7315853beae9f8dee82e48
Austin-Cook <austin.cook.id@gmail.com>
2024-01-27 16:03:18 -0700

    Implement hybrid encryption

--- EncryptedEchoClient.cs
+++ EncryptedEchoClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
@@ -17,12 +18,29 @@ internal sealed class EncryptedEchoClient : EchoClientBase
     /// <inheritdoc />
     public EncryptedEchoClient(ushort port, string address) : base(port, address) { }
 
+    private byte[]? serverPublicKey = null;
+
     /// <inheritdoc />
     public override void ProcessServerHello(string message)
     {
         // todo: Step 1: Get the server's public key. Decode using Base64.
         // Throw a CryptographicException if the received key is invalid.
-        
+
+        // Console.WriteLine(message);
+
+        serverPublicKey = Convert.FromBase64String(message);
+
+        try
+        {
+            using RSACryptoServiceProvider rsa = new();
+            rsa.ImportRSAPublicKey(serverPublicKey, out _);
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.Message);
+            Environment.Exit(0);
+        }
+
     }
 
     /// <inheritdoc />
@@ -30,23 +48,69 @@ internal sealed class EncryptedEchoClient : EchoClientBase
     {
         byte[] data = Settings.Encoding.GetBytes(input);
 
-        // todo: Step 1: Encrypt the input using hybrid encryption.
+        // Step 1: Encrypt the input using hybrid encryption.
         // Encrypt using AES with CBC mode and PKCS7 padding.
         // Use a different key each time.
 
-        // todo: Step 2: Generate an HMAC of the message.
+        using Aes aes = Aes.Create();
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
+        // create an encryptor to perform the stream transform
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        // create the streams used for encryption
+        using MemoryStream msEncrypt = new();
+        using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
+        using StreamWriter swEncrypt = new(csEncrypt);
+        // write all data to the stream.
+        swEncrypt.Write(input);
+        swEncrypt.Flush();
+        csEncrypt.FlushFinalBlock();
+        byte[] encryptedInput = msEncrypt.ToArray();
+
+        Console.WriteLine("aes key in client: " + BitConverter.ToString(aes.Key)); // .Replace("-", "")
+        Console.WriteLine("Message right after encryption: " + BitConverter.ToString(encryptedInput));
+
+        // Step 2: Generate an HMAC of the message.
         // Use the SHA256 variant of HMAC.
         // Use a different key each time.
+        using HMACSHA256 hmac = new();
+        byte[] messageHash = hmac.ComputeHash(Encoding.Unicode.GetBytes(input));
 
-        // todo: Step 3: Encrypt the message encryption and HMAC keys using RSA.
+        // Step 3: Encrypt the message encryption and HMAC keys using RSA.
         // Encrypt using the OAEP padding scheme with SHA256.
+        byte[] aesKey = aes.Key;
+        byte[] hmacKey = hmac.Key;
 
-        // todo: Step 4: Put the data in an EncryptedMessage object and serialize to JSON.
+        byte[]? aesKeyEncrypted = null;
+        byte[]? hmacKeyEncrypted = null;
+
+        try
+        {
+            // Create a new instance of RSACryptoServiceProvider
+            using RSACryptoServiceProvider rsa = new();
+
+            // Import the RSA Key information. This only needs
+            // to include the public key information.
+            rsa.ImportRSAPublicKey(serverPublicKey, out _);
+
+            // Encrypt the byte array with OAEP padding
+            aesKeyEncrypted = rsa.Encrypt(aesKey, true);
+            hmacKeyEncrypted = rsa.Encrypt(hmacKey, true);
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.Message);
+            Environment.Exit(0);
+        }
+
+        // Step 4: Put the data in an EncryptedMessage object and serialize to JSON.
         // Return that JSON.
         // var message = new EncryptedMessage(...);
         // return JsonSerializer.Serialize(message);
+        EncryptedMessage encryptedMessage = new(aesKeyEncrypted, aes.IV, encryptedInput, hmacKeyEncrypted, messageHash);
+        Console.WriteLine("Hash in client before sending: " + Encoding.Unicode.GetString(encryptedMessage.HMAC));
 
-        return input;
+        return JsonSerializer.Serialize(encryptedMessage);
     }
 
     /// <inheritdoc />
--- EncryptedEchoServer.cs
+++ EncryptedEchoServer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
@@ -11,8 +12,19 @@ internal sealed class EncryptedEchoServer : EchoServerBase
     private ILogger<EncryptedEchoServer> Logger { get; init; } =
         Settings.LoggerFactory.CreateLogger<EncryptedEchoServer>()!;
 
+    private byte[]? publicKeyPKCS1 = null;
+    private byte[]? privateKeyPKCS1 = null;
+
     /// <inheritdoc />
-    internal EncryptedEchoServer(ushort port) : base(port) { }
+    internal EncryptedEchoServer(ushort port) : base(port)
+    {
+        using RSACryptoServiceProvider rsa = new();
+
+        // save the public and private keys in PKCS#1 format
+        publicKeyPKCS1 = rsa.ExportRSAPublicKey();
+        privateKeyPKCS1 = rsa.ExportRSAPrivateKey();
+    }
+
 
     // todo: Step 1: Generate a RSA key (2048 bits) for the server.
 
@@ -22,24 +34,105 @@ internal sealed class EncryptedEchoServer : EchoServerBase
         // todo: Step 1: Send the public key to the client in PKCS#1 format.
         // Encode using Base64: Convert.ToBase64String
 
-        return String.Empty;
+        // ensure the public key is set
+        if (publicKeyPKCS1 == null)
+        {
+            Environment.Exit(0);
+        }
+
+        string publicKeyPKCS1Base64 = Convert.ToBase64String(publicKeyPKCS1);
+
+        return publicKeyPKCS1Base64;
     }
 
     /// <inheritdoc />
     public override string TransformIncomingMessage(string input)
     {
-        // todo: Step 1: Deserialize the message.
+        // Step 1: Deserialize the message.
         // var message = JsonSerializer.Deserialize<EncryptedMessage>(input);
+        EncryptedMessage encryptedMessage = JsonSerializer.Deserialize<EncryptedMessage>(input);
 
-        // todo: Step 2: Decrypt the message using hybrid encryption.
+        // Step 2: Decrypt the message using hybrid encryption.
 
-        // todo: Step 3: Verify the HMAC.
+        // decrypt the keys
+        byte[]? aesKey = null;
+        byte[]? hmacKey = null;
+        try
+        {
+            // Create a new instance of RSACryptoServiceProvider.
+            using RSACryptoServiceProvider rsa = new();
+            // Import the RSA Key information. This needs
+            // to include the private key information.
+            rsa.ImportRSAPrivateKey(privateKeyPKCS1, out _);
+
+            // Decrypt the byte array with OAEP padding.
+            aesKey = rsa.Decrypt(encryptedMessage.AesKeyWrap, true);
+
+            Console.WriteLine("aes key received by server: " + BitConverter.ToString(aesKey)); // .Replace("-", "")
+            hmacKey = rsa.Decrypt(encryptedMessage.HMACKeyWrap, true);
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.ToString());
+            Environment.Exit(0);
+        }
+
+
+        Console.WriteLine("Message right before decryption: " + Encoding.Unicode.GetString(encryptedMessage.Message));
+
+        // decrypt the message
+        using Aes aes = Aes.Create();
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
+        aes.Key = aesKey;
+        aes.IV = encryptedMessage.AESIV;
+        // Create a decryptor to perform the stream transform.
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        // Create the streams used for decryption.
+        using MemoryStream msDecrypt = new(encryptedMessage.Message);
+        using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using StreamReader srDecrypt = new(csDecrypt);
+        // Read the decrypted bytes from the decrypting stream
+        // and place them in a string.
+        string plaintext = srDecrypt.ReadToEnd();
+
+        // DELETEME
+        Console.WriteLine("Plaintext: " + plaintext);
+
+
+        // Step 3: Verify the HMAC.
         // Throw an InvalidSignatureException if the received hmac is bad.
+        using HMACSHA256 hmac = new(hmacKey);
+        byte[] localHash = hmac.ComputeHash(Encoding.Unicode.GetBytes(plaintext));
+        Console.WriteLine("Hash from client: " + Encoding.Unicode.GetString(encryptedMessage.HMAC));
+        Console.WriteLine("Local hash      : " + Encoding.Unicode.GetString(localHash));
 
-        // todo: Step 3: Return the decrypted and verified message from the server.
+        // check for equality
+        bool match = true;
+        if (localHash.Length != encryptedMessage.HMAC.Length)
+        {
+            match = false;
+        }
+        for (int i = 0; i < localHash.Length; i++)
+        {
+            if (localHash[i] != encryptedMessage.HMAC[i])
+            {
+                match = false;
+                Console.WriteLine("inequality found: " + localHash[i] + ", " + encryptedMessage.HMAC[i]);
+            }
+        }
+        if (!match)
+        {
+            throw new InvalidSignatureException("The two hashes are not equal");
+        }
+        // if (!localHash.SequenceEqual(encryptedMessage.HMAC)) {
+        //     throw new InvalidSignatureException("The two hashes are not equal");
+        // }
+
+        // Step 4: Return the decrypted and verified message from the server.
         // return Settings.Encoding.GetString(decryptedMessage);
 
-        return input;
+        return plaintext;
     }
 
     /// <inheritdoc />
